43deaf1fc90cc612a2e3bd7dc10397002d4e8744
Symbol <Symbolistic@live.com>
2020-05-19 12:02:47 -0400

    Updates

---
+++ precache-manifest.49dfb5fa4f675f7a07e671c96505dfb6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7091a84155646df23249994277280889",
+    "url": "/Random-Quote-Machine-NEW/index.html"
+  },
+  {
+    "revision": "140a4c55c812930d1320",
+    "url": "/Random-Quote-Machine-NEW/static/css/main.3e3e2772.chunk.css"
+  },
+  {
+    "revision": "ef6cc14dfe381a7fd160",
+    "url": "/Random-Quote-Machine-NEW/static/js/2.5ec1e6ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine-NEW/static/js/2.5ec1e6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140a4c55c812930d1320",
+    "url": "/Random-Quote-Machine-NEW/static/js/main.0156eb5e.chunk.js"
+  },
+  {
+    "revision": "dee599960e99bf1aa293",
+    "url": "/Random-Quote-Machine-NEW/static/js/runtime-main.f78c72eb.js"
+  },
+  {
+    "revision": "58ea257b5da11397cfc8ffe399e9f926",
+    "url": "/Random-Quote-Machine-NEW/static/media/1.58ea257b.jpg"
+  },
+  {
+    "revision": "76249a80d90edb2b39fde9cf3577099f",
+    "url": "/Random-Quote-Machine-NEW/static/media/2.76249a80.jpg"
+  },
+  {
+    "revision": "1a267c8d6d137f2c67fc7ff2ce287bd1",
+    "url": "/Random-Quote-Machine-NEW/static/media/3.1a267c8d.jpg"
+  },
+  {
+    "revision": "53a4357387926612ae08f93291973d31",
+    "url": "/Random-Quote-Machine-NEW/static/media/4.53a43573.jpg"
+  },
+  {
+    "revision": "e6d00bc64f8085494d81f054192f8b5e",
+    "url": "/Random-Quote-Machine-NEW/static/media/5.e6d00bc6.jpg"
+  },
+  {
+    "revision": "4ee067b1259bc421b4279193e8514eb9",
+    "url": "/Random-Quote-Machine-NEW/static/media/6.4ee067b1.jpg"
+  },
+  {
+    "revision": "67203b9cc260ea3506174a536472aa07",
+    "url": "/Random-Quote-Machine-NEW/static/media/7.67203b9c.jpg"
+  },
+  {
+    "revision": "ab3870956c55ce7d4a4aa4899abd1258",
+    "url": "/Random-Quote-Machine-NEW/static/media/8.ab387095.jpg"
+  },
+  {
+    "revision": "7fe47125c281525e82e24ef03feddaad",
+    "url": "/Random-Quote-Machine-NEW/static/media/9.7fe47125.jpg"
+  },
+  {
+    "revision": "4a3f5f9140ca3e985c715017309ecc0b",
+    "url": "/Random-Quote-Machine-NEW/static/media/loading.4a3f5f91.jpeg"
+  }
+]);
